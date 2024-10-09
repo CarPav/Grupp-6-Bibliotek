388300f19025415601d527571f8c13f6467dd1f4
davidperjans <perjansdavid@gmail.com>
2024-10-09 12:23:50 +0200

    La till Books klassen

---
+++ Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GASSIGN_Bibliotekapp
+{
+    public class Books
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public int ISBN { get; set; }
+        public bool isCheckedOut { get; set; }
+    }
+}
