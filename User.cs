3bf741cfc06c649f6f8399acc5a32dc17f5c3e75
davidperjans <perjansdavid@gmail.com>
2024-10-11 11:26:12 +0200

    Har strukturerat projektet skapat alla klasser och har även grundat metoderna så bara att implementera med sina egna funktioner.

---
+++ User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GASSIGN_Bibliotekapp
+{
+    public class User
+    {
+        //Som standard har jag gjort alla metoder likadana. Märker ni att eran metod kanske ska ta emot parametrar eller liknande så ändrar ni ju bara på det såklart!
+        public void AddBook()
+        {
+            //Caroline lägger till sin kod här
+        }
+
+        public void RemoveBookByTitle()
+        {
+            //Felicia lägger till sin kod här
+        }
+
+        public void SearchBookByAuthor()
+        {
+            //David lägger till sin kod här
+        }
+
+        public void ShowAllBooks()
+        {
+            //Andreas lägger till sin kod här
+        }
+
+        public void CheckoutAndReturnBook()
+        {
+            //Ayub lägger till sin kod här
+        }
+    }
+}
---
+++ UserInterface.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GASSIGN_Bibliotekapp
+{
+    public class UserInterface
+    {
+        public static void PrintStartMenu()
+        {
+            Console.WriteLine("--- Bibliotekhanteringssytem ---");
+            while (true)
+            {
+                Console.WriteLine("\n1. Lägg till en ny bok");
+                Console.WriteLine("2. Ta bort en bok enligt titel");
+                Console.WriteLine("3. Sök efter en bok enligt författare");
+                Console.WriteLine("4. Visa alla böcker i samlingen");
+                Console.WriteLine("5. Checka ut / Returnera boken");
+                Console.WriteLine("6. Avsluta programmet");
+                Console.Write("Ange ditt val: ");
+                string userOption = Console.ReadLine()!;
+
+                switch (userOption)
+                {
+                    case "1":
+                        //AddBook();
+                        break;
+                    case "2":
+                        //RemoveBookByTitle();
+                        break;
+                    case "3":
+                        //SearchBookByAuthor();
+                        break;
+                    case "4":
+                        //ShowAllBooks();
+                        break;
+                    case "5":
+                        //CheckoutAndReturnBook();
+                        break;
+                    case "6":
+                        Console.WriteLine("Tack för du använde Bibliotekhanteringssytemet");
+                        return;
+                    default:
+                        Console.WriteLine("Ogiltligt val, försök igen");
+                        break;
+                }
+            }
+        }
+    }
+}
